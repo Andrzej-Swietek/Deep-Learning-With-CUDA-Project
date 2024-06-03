7af581795cd1fd7eebab5bd3ee41cc44556cf8ae
Andrzej Świętek <mr.andi321@gmail.com>
2024-06-03 21:53:21 +0200

    Added ResNet architecture

---
+++ models/resnet.py
@@ -0,0 +1,91 @@
+from collections import namedtuple
+import torch
+from torchvision import models
+from torch import nn
+
+
+class cbrblock(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super(cbrblock, self).__init__()
+        self.cbr = nn.Sequential(nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=(1, 1),
+                                           padding='same', bias=False),
+                                 nn.BatchNorm2d(output_channels),
+                                 nn.ReLU()
+                                 )
+
+    def forward(self, x):
+        out = self.cbr(x)
+        return out
+
+
+class conv_block(nn.Module):
+    def __init__(self, input_channels, output_channels, scale_input):
+        super(conv_block, self).__init__()
+        self.scale_input = scale_input
+        if self.scale_input:
+            self.scale = nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=(1, 1),
+                                   padding='same')
+        self.layer1 = cbrblock(input_channels, output_channels)
+        self.dropout = nn.Dropout(p=0.01)
+        self.layer2 = cbrblock(output_channels, output_channels)
+
+    def forward(self, x):
+        residual = x
+        out = self.layer1(x)
+        out = self.dropout(out)
+        out = self.layer2(out)
+        if self.scale_input:
+            residual = self.scale(residual)
+        out = out + residual
+
+        return out
+
+
+# Overall network - READ IT, STUDY IT, TAKE YOUR TIME HERE!!
+class WideResNet(nn.Module):
+    def __init__(self, num_classes):
+        super(WideResNet, self).__init__()
+        nChannels = [1, 16, 160, 320, 640]
+
+        self.input_block = cbrblock(nChannels[0], nChannels[1])
+
+        # Module with alternating components employing input scaling
+        self.block1 = conv_block(nChannels[1], nChannels[2], 1)
+        self.block2 = conv_block(nChannels[2], nChannels[2], 0)
+        self.pool1 = nn.MaxPool2d(2)
+        self.block3 = conv_block(nChannels[2], nChannels[3], 1)
+        self.block4 = conv_block(nChannels[3], nChannels[3], 0)
+        self.pool2 = nn.MaxPool2d(2)
+        self.block5 = conv_block(nChannels[3], nChannels[4], 1)
+        self.block6 = conv_block(nChannels[4], nChannels[4], 0)
+
+        # Global average pooling
+        self.pool = nn.AvgPool2d(7)
+
+        # Feature flattening followed by linear layer
+        self.flat = nn.Flatten()
+        self.fc = nn.Linear(nChannels[4], num_classes)
+
+    def forward(self, x):
+        out = self.input_block(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.pool1(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = self.pool2(out)
+        out = self.block5(out)
+        out = self.block6(out)
+        out = self.pool(out)
+        out = self.flat(out)
+        out = self.fc(out)
+
+        return out
+
+
+"""
+pretrained_weights_path = 'trained_wideresnet.pth'
+model = WideResNetForNST(num_classes=1000, pretrained_weights_path=pretrained_weights_path, requires_grad=False)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+"""
